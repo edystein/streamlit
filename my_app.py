8ae8ceb18aa915ec7614dae5d885bc0e2a615b8a
edys <edy.stein@gmail.com>
2022-09-23 22:44:45 +0300

    minor

--- my_app.py
+++ my_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""#My first app!
+st.write("""
+#My first app!
+
 Hello *world!*""")
 df = pd.DataFrame({'x': [1, 2, 3], 'y': [10, 20, 30]})
 st.line_chart(df)
